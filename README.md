ae2a7d0871ca3399556d2c494cb784441d84054d
gingerhale <haleydickson5@gmail.com>
2022-10-25 20:31:25 -0600

    tweaks

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cpnt262-a1
+# CPNT262
+- Assignment 1 : Push of a button
+- Haley Dickson
+- GH Pages: 
--- client.js
+++ client.js
@@ -1,22 +1,22 @@
 
-const searchList = function(){
+const searchList = function () {
   const names = document.querySelector('.people');
   const answer = document.querySelector('.answer');
-  
+
   const searchTerm = prompt('Are you invited? Enter your name to find out');
-  
+
   const result = names.innerHTML.includes(searchTerm);
-  
+
   if (result) {
     answer.innerHTML = "Yes, don't forget Haley's Gift!"
   } else {
     answer.innerHTML = "Nope! Invite must've been lost in the mail"
   }
-  
+
   names.innerHTML = names.innerHTML.replace(searchTerm, `<strong>${searchTerm}</strong>`);
-  
-  }
-  
-  const button = document.querySelector('button');
-  
-  button.addEventListener('click', searchList);
+
+}
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', searchList);
